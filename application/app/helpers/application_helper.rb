fe640ed1938e02613f553079a03528fb4740023c
ceekays <kachaleedmond@gmail.com>
2011-05-11 13:35:13 +0200

    Added 'container_id' to 'img_button_submit_to' method

--- application/app/helpers/application_helper.rb
+++ application/app/helpers/application_helper.rb
@@ -8,10 +8,14 @@ module ApplicationHelper
   end
 
   def img_button_submit_to(url, image, options = {}, params = {})
+    form_id       = (options[:form_id]) ? ("#{options[:form_id]}") : "frm_general"
+    container_id  = (options[:container_id]) ? ("#{options[:container_id]}") : ""
+    confirmation_message = ""
+    confirmation_message = "onclick = \"return confirmRecordDeletion('#{options[:confirm]}', '#{form_id}', '#{container_id}')\"" if options[:confirm]
+
     content = ""
-    content << "<form " + ((options[:form_id])?("id=#{options[:form_id]}"):"id='frm_general'") + " method='post' action='#{url}'><input type='image' src='#{image}' " +
-      ((options[:confirm])?("onclick=\"return confirmRecordDeletion('" +
-      options[:confirm] + "', '" + ((options[:form_id])?("#{options[:form_id]}"):"frm_general") + "')\""):"") + "/>"
+    content << "<form id='#{form_id}' method='post' action='#{url}'>"
+    content << "<input type='image' src='#{image}' #{confirmation_message}/>"
 
     params.each {|n,v| content << "<input type='hidden' name='#{n}' value='#{v}'/>" }
     content << "</form>"
