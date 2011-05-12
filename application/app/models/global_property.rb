db40f245ed64ebadf58aa6eeeda734732207a902
Precious <precious.bondwe@googlemail.com>
2011-05-12 16:46:42 +0200

    added actions to generate next ivr access code and next call id

--- application/app/models/global_property.rb
+++ application/app/models/global_property.rb
@@ -5,5 +5,26 @@ class GlobalProperty < ActiveRecord::Base
 
   def to_s
     return "#{property}: #{property_value}"
-  end  
+  end
+
+  def self.next_ivr_access_code
+    site_location = Location.current_location.site_id.rjust(3,'0')
+
+    globalproperty = GlobalProperty.find_by_property("IVR_ACCESS_CODE_COUNTER")
+    current_number = globalproperty.property_value.to_i + 1
+    globalproperty.update_attribute('property_value', current_number)
+    globalproperty.save
+
+    return site_location.to_s + current_number.to_s.rjust(5,'0')
+  end
+
+  def self.next_call_id
+    globalproperty = GlobalProperty.find_by_property("CALL_ID_COUNTER")
+    current_number = globalproperty.property_value.to_i + 1
+    globalproperty.update_attribute('property_value', current_number)
+    globalproperty.save
+
+    return  current_number.to_s.rjust(8,'0')
+  end
+  
 end
