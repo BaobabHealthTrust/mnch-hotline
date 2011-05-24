64702a979f4ac3bc0671325c15ae9d3fbdef5248
ceekays <kachaleedmond@gmail.com>
2011-05-24 15:14:25 +0200

    Added an MNCH Task list migration

---
+++ application/db/migrate/20110519153717_generate_mnch_tasks_list.rb
@@ -0,0 +1,56 @@
+class GenerateMnchTasksList < ActiveRecord::Migration
+  def self.up
+    encounter_types = ["REGISTRATION", "MATERNAL HEALTH SYMPTOMS",
+                        "PREGNANCY STATUS", "CHILD HEALTH SYMPTOMS",
+                        "UPDATE OUTCOME", "TIPS AND REMINDERS", nil]
+
+    urls = ["/encounters/new/registration?patient_id={patient}",
+            "/encounters/new/female_symptoms?patient_id={patient}",
+            "/encounters/new/pregnancy_status?patient_id={patient}", 
+            "/encounters/new/child_symptoms?patient_id={patient}", 
+            "/encounters/new/outcome?patient_id={patient}",
+            "/encounters/new/tips_and_reminders?patient_id={patient}",
+            "/patients/show/{patient}"]
+
+    locations = ["*","MNCH Hotline Station", "MNCH Hotline Station",
+                  "MNCH Hotline Station", "MNCH Hotline Station",
+                  "MNCH Hotline Station","*"]
+
+    has_obs_scopes  = ["*", "TODAY", "TODAY", "TODAY",
+                        "TODAY", "TODAY", "*"]
+
+    skips_if_has    = ["0", "0", "0", "0", "0", "0", "0"]
+
+    sort_weights    = ["1", "2", "3", "4", "5", "6", "1000"]
+    
+    genders         = [nil, "F", "F", "*", nil, nil, nil]
+
+    descriptions    = ["Registration for every location",
+                        "Get female health symptoms for all female adult patients",
+                        "Get pregnancy status if the patient is female adult",
+                        "Get child health symptoms if the patient is a child (either gender)",
+                        "Record an outcome if symptoms have been collected",
+                        "Record tips and reminders preferences",
+                        "Everything is finished, go to the patient dashboard"]
+
+    encounter_types.each_with_index do |encounter_type, index|
+      task                = Task.new()
+      task.url            = urls[index]
+      task.description    = descriptions[index]
+      task.encounter_type = encounter_type
+      task.location       = locations[index]
+      task.has_obs_scope  = has_obs_scopes[index]
+      task.gender         = genders[index]
+      task.skip_if_has    = skips_if_has[index]
+      task.sort_weight    = sort_weights[index]
+      task.creator        = 1
+      task.save
+    end
+  
+  end
+
+  def self.down
+    all_tasks = Task.all(:conditions => ["location =?", "MNCH Hotline Station"], :order => 'sort_weight ASC')
+    all_tasks.each{|task| task.delete}
+  end
+end
