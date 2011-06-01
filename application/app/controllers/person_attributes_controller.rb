95e4b147ec50c994569bb8e51bbc32f66434b20b
ceekays <kachaleedmond@gmail.com>
2011-06-01 22:08:43 +0200

    Added method to retrieve patient's nearest health facility

--- application/app/controllers/person_attributes_controller.rb
+++ application/app/controllers/person_attributes_controller.rb
@@ -1,4 +1,8 @@
 class PersonAttributesController < ApplicationController
+  def nearest_health_facility
+    search("value", params[:search_string], "NEAREST HEALTH FACILITY")
+  end
+
   def search(field_name, search_string, attribute_type)
     most_common_values = PersonAttribute.find_most_common(field_name, search_string, attribute_type)
     most_common_values = most_common_values.map{|attribute| attribute.send(field_name)}
