824450b41ab80a20fcc10c11389975093ce74562
ceekays <kachaleedmond@gmail.com>
2011-06-01 22:07:11 +0200

    Added an ajax search helping method

--- application/app/controllers/person_attributes_controller.rb
+++ application/app/controllers/person_attributes_controller.rb
@@ -1,2 +1,8 @@
 class PersonAttributesController < ApplicationController
+  def search(field_name, search_string, attribute_type)
+    most_common_values = PersonAttribute.find_most_common(field_name, search_string, attribute_type)
+    most_common_values = most_common_values.map{|attribute| attribute.send(field_name)}
+
+    render :text => "<li>" + most_common_values.join("</li><li>") + "</li>"
+  end
 end
