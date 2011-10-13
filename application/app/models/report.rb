205a99501b50c2fed26cf41a180426813fa874ec
Edmond Kachale <kachaleedmond@gmail.com>
2011-10-13 03:34:11 +0200

    Added a method for generating 'display date ranges'

--- application/app/models/report.rb
+++ application/app/models/report.rb
@@ -28,4 +28,40 @@ module Report
     report_date_ranges
   end
 
+  def self.generate_grouping_date_ranges(grouping, start_date, end_date)
+    start_date  = start_date.to_date
+    end_date    = end_date.to_date
+
+    grouping_date_ranges  = {:display_text => nil, :date_ranges => []}
+
+    case grouping
+      when "week"
+        grouping_date_ranges[:display_text] = "Week beginning XXXX ending  YYYY"
+
+        current_week  = start_date.beginning_of_week
+        final_week    = end_date.beginning_of_week
+
+        begin
+          week_beginning  = current_week.beginning_of_week
+          week_ending     = current_week.end_of_week
+          grouping_date_ranges[:date_ranges].push([week_beginning.strftime("%Y-%m-%d"), week_ending.strftime("%Y-%m-%d")])
+          current_week    += 1.week
+        end while current_week <= final_week
+
+      when "month"
+        grouping_date_ranges[:display_text]  = "Month beginning XXXX ending  YYYY"
+        final_month   = end_date.beginning_of_month
+        current_month = start_date.beginning_of_month
+
+        begin
+          month_beginning  = current_month.beginning_of_month
+          month_ending     = current_month.end_of_month
+          grouping_date_ranges[:date_ranges].push([month_beginning.strftime("%Y-%m-%d"), month_ending.strftime("%Y-%m-%d")])
+          current_month    += 1.month
+        end while current_month <= final_month
+    end
+
+    return grouping_date_ranges
+  end
+
 end
