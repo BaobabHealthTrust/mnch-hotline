ec00a61b72f1550593d8326418232dfdb44c1126
gamphani <dave.phiri@baobabhealth.org>
2011-11-16 16:33:04 +0200

    Added space to avoid preselection of 'Yes'

--- application/app/helpers/touchscreen_helper.rb
+++ application/app/helpers/touchscreen_helper.rb
@@ -110,7 +110,7 @@ module TouchscreenHelper
     options = {
       :tt_pageStyleClass => "NoKeyboard"
     }.merge(options)                 
-    touch_select_tag(concept, patient, options_for_select([['Yes','YES'],['No','NO']], value), options, time)
+    touch_select_tag(concept, patient, options_for_select([['',''],['Yes','YES'],['No','NO']], value), options, time)
   end
   
   def touch_hidden_tag(concept, patient, value, options={}, time=DateTime.now())
