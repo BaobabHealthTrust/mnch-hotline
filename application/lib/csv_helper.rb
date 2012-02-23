9fc465d3e9e076b88e7536993b8aa3d32d306636
Catchit <precious.bondwe@baobabhealth.org>
2012-02-23 12:41:36 +0200

    added patient_demographics csv export

--- application/lib/csv_helper.rb
+++ application/lib/csv_helper.rb
@@ -4,21 +4,39 @@ module CSVHelper
 
   private
 
-  def export_to_csv(report_name, report_headers, report_data)
+  def export_to_csv(report_name, report_headers, report_data, patient_type)
     config = YAML.load_file("config/report.yml")
     @export_path = config["config"]["export_path"]
 
-    output_file = report_name.downcase + ".csv"
+    output_file = report_name.downcase + "_#{rand(1000000)}" + ".csv"
     output_file = @export_path + "/" + output_file
 
     case report_name
     when 'patient_demographics_report'
       FasterCSV.open(output_file, 'w',:headers => report_headers) do |csv|
       csv << report_headers
-        10.times do
-          csv << ["precious","bondwe"]
-        end
-        
+         report_data.reverse.map do |data|
+           csv << ["beginning : #{data[:start_date]} ending : #{data[:end_date]}","",""]
+           csv << ["New Registrations","","","#{data[:new_registrations]}"]
+           data[:catchment].map do | catchment |
+             csv << ["","#{catchment.first}","#{catchment.last}",""]
+           end
+             csv << ["","","",""]
+           if patient_type.downcase == 'children'
+              data[:gender].map do | gender |
+                csv << ["#{gender.first.humanize}","","#{gender.last}",""]
+              end
+           elsif patient_type.downcase == 'women'
+              data[:pregnancy_status].map do | pregnancy_status |
+                csv << ["#{pregnancy_status.first.humanize}","","#{pregnancy_status.last}",""]
+              end
+           elsif patient_type.downcase == 'all'
+              data[:patient_type].map do | patient |
+                csv << ["#{patient.first.humanize}","","#{patient.last}",""]
+              end
+           end
+           csv << ["","","",""]
+         end
       end
       
     when 'patient_health_issues_report'
