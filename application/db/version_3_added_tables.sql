d837de29b9bc41ab01a1b51d9187a6ab5e2d47ee
Catchit <precious.bondwe@baobabhealth.org>
2013-06-05 11:23:31 +0200

    added missing tables to the upgrade script

--- application/db/version_3_added_tables.sql
+++ application/db/version_3_added_tables.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.5.31, for debian-linux-gnu (i686)
 --
--- Host: localhost    Database: migrated_mnch
+-- Host: localhost    Database: hotline_new
 -- ------------------------------------------------------
--- Server version	5.5.31-0ubuntu0.12.04.1
+-- Server version	5.5.31-0ubuntu0.12.04.1-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -167,6 +167,30 @@ INSERT INTO `village` VALUES (31872,'Muocha ',341,1,'2011-09-27 00:00:00',0,NULL
 UNLOCK TABLES;
 
 --
+-- Table structure for table `village_headman`
+--
+
+DROP TABLE IF EXISTS `village_headman`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `village_headman` (
+  `village_headman_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `traditional_authority_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`village_headman_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `village_headman`
+--
+
+LOCK TABLES `village_headman` WRITE;
+/*!40000 ALTER TABLE `village_headman` DISABLE KEYS */;
+/*!40000 ALTER TABLE `village_headman` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `health_center`
 --
 
@@ -200,4 +224,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-05-02 17:03:33
+-- Dump completed on 2013-06-05 11:20:52
