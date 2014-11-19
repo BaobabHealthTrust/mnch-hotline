db871b85a68df7670405b478a8cc7f0d26030ca0
blessings <blessings.mhango@gmail.com>
2014-11-19 15:19:04 +0200

    late for ANC followup call

---
+++ application/app/models/hsa_village.rb
@@ -0,0 +1,6 @@
+class HsaVillage < ActiveRecord::Base
+  has_many :users, :foreign_key => :hsa_id
+  has_many :villages, :foreign_key => :village_id   
+  has_many :healthcenters, :foreign_key => :heath_center_id
+  has_many :districts, :foreign_key => :district_id
+end
