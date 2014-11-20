590f85b057332a0a077d39f4868c0f73c12cb817
blessings <blessings.mhango@gmail.com>
2014-11-20 10:43:07 +0200

    association with user

--- application/app/models/hsa_village.rb
+++ application/app/models/hsa_village.rb
@@ -1,5 +1,5 @@
 class HsaVillage < ActiveRecord::Base
-  has_many :users, :foreign_key => :hsa_id
+  has_many :users, :foreign_key => :user_id
   has_many :villages, :foreign_key => :village_id   
   has_many :healthcenters, :foreign_key => :heath_center_id
   has_many :districts, :foreign_key => :district_id
