c58ff98f1ccc89514dcf9bc1f32fbb81c7eef6ee
blessings <blessings.mhango@gmail.com>
2014-12-08 09:19:50 +0200

    using current village not home village

--- application/app/controllers/clinic_controller.rb
+++ application/app/controllers/clinic_controller.rb
@@ -395,9 +395,9 @@ class ClinicController < ApplicationController
           follow_up[:family_name] = person.family_name
           follow_up[:given_name] = person.given_name
           follow_up[:family_name_prefix] = person.family_name_prefix
-          follow_up[:address2] = person.address2
+          follow_up[:current_residence] = person.city_village
           follow_up[:gestation_age] = person.gestation_age
-          village = Village.find_by_name_and_county_district(person.address2, person.county_district)
+          village = Village.find_by_name_and_county_district(person.city_village, person.county_district)
           hsa_village = HsaVillage.find_by_village_id(village.village_id) rescue nil
           person_name = PersonName.find_by_person_id(hsa_village.hsa_id) rescue nil
           follow_up[:hsa_name] = person_name.given_name + " " + person_name.family_name rescue nil
