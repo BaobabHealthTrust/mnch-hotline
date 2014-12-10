7ad521250db48d228ed05cf766ebec1d07c8b02b
blessings <blessings.mhango@gmail.com>
2014-12-10 15:36:15 +0200

    updated late delivery query

--- application/app/models/follow_up.rb
+++ application/app/models/follow_up.rb
@@ -88,6 +88,10 @@ class FollowUp < ActiveRecord::Base
                                                           AND oo.voided = 0 
                                                           GROUP BY ee.patient_id
                                                           HAVING COUNT(ee.patient_id) < 4)
+                                      AND e.patient_id NOT IN (SELECT patient_id 
+                                                               FROM follow_up 
+                                                               WHERE date_created >= '#{start_date} 00:00' 
+                                                               AND date_created <='#{current_date} 23:59')
                                       GROUP BY e.patient_id
                                       ORDER BY e.encounter_datetime DESC")
   
@@ -110,8 +114,6 @@ class FollowUp < ActiveRecord::Base
     encounter_type = EncounterType.find_by_name('PREGNANCY STATUS').id
     concept_id = ConceptName.find_by_name('Expected due date').concept_id
     anc_connect_program_id = Program.find_by_name('ANC CONNECT PROGRAM').program_id
-    next_visit_date_concept_id = ConceptName.find_by_name('Next ANC Visit Date').concept_id
-    anc_encounter_type = EncounterType.find_by_name("ANC VISIT").id
     
     patients = Encounter.find_by_sql("SELECT e.patient_id, pn.given_name,pn.family_name,pn.family_name_prefix,
                                       pa.city_village,o.concept_id,o.value_text,
@@ -128,20 +130,9 @@ class FollowUp < ActiveRecord::Base
                                       WHERE e.encounter_type = #{encounter_type}
                                       AND pp.program_id = #{anc_connect_program_id}
                                       AND o.concept_id = #{concept_id} and o.value_text IS NOT NULL 
-                                      AND floor((280 - (DATE(o.value_text) - curdate()))/7) < 42 
-                                      AND floor((280 - (DATE(o.value_text) - curdate()))/7) > 0
+                                      AND floor((280 - (DATE(o.value_text) - curdate()))/7) > 0 
+                                      AND floor((280 - (DATE(o.value_text) - curdate()))/7) < 42
                                       AND e.voided = 0
-                                      AND e.patient_id IN(SELECT ee.patient_id 
-                                                          FROM encounter ee 
-                                                          INNER JOIN obs oo 
-                                                          ON ee.encounter_id = oo.encounter_id
-                                                          WHERE ee.encounter_type = #{anc_encounter_type}
-                                                          AND oo.concept_id = #{next_visit_date_concept_id}
-                                                          AND DATE(oo.value_text) <= '#{last_anc_date} 23:59'
-                                                          AND ee.encounter_datetime <= '#{start_date} 23:59'
-                                                          AND ee.voided = 0 
-                                                          GROUP BY ee.patient_id
-                                                          HAVING COUNT(ee.patient_id) < 4)
                                       GROUP BY e.patient_id
                                       ORDER BY e.encounter_datetime DESC")
   
@@ -214,6 +205,7 @@ class FollowUp < ActiveRecord::Base
     encounter_type = EncounterType.find_by_name('PREGNANCY STATUS').id
     concept_id = ConceptName.find_by_name('Expected due date').concept_id
     anc_connect_program_id = Program.find_by_name('ANC CONNECT PROGRAM').program_id
+    baby_encounter_type = EncounterType.find_by_name('BABY DELIVERY').id
     
     patients = Encounter.find_by_sql("SELECT e.patient_id, pn.given_name,pn.family_name,pn.family_name_prefix,
                                       pa.city_village,o.concept_id,o.value_text,
@@ -231,10 +223,15 @@ class FollowUp < ActiveRecord::Base
                                       AND pp.program_id = #{anc_connect_program_id}
                                       AND o.concept_id = #{concept_id} and o.value_text IS NOT NULL 
                                       AND floor((280 - (DATE(o.value_text) - curdate()))/7) >= 42 
-                                      AND floor((280 - (DATE(o.value_text) - curdate()))/7) > 0
+                                      AND floor((280 - (DATE(o.value_text) - curdate()))/7) <= 46
+                                      AND e.patient_id NOT IN (SELECT ee.patient_id 
+                                                               FROM encounter ee 
+                                                               WHERE ee.encounter_type = #{encounter_type} 
+                                                               AND date_created >= '#{start_date} 00:00' 
+                                                               AND date_created <='#{current_date} 23:59')
                                       AND e.voided = 0
                                       GROUP BY e.patient_id
-                                      ORDER BY e.encounter_datetime DESC;")
+                                      ORDER BY floor((280 - (DATE(o.value_text) - curdate()))/7) DESC;")
                                       
     data = patients.select{|p| HsaVillage.is_patient_village_in_anc_connect(p.patient_id, district)}
     return data
