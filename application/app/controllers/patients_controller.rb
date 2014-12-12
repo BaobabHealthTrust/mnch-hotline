af9506c9906bb4cb8a6e0422cb899494831f8322
blessings <blessings.mhango@gmail.com>
2014-12-12 16:25:59 +0200

    checking if patient is in anc connect program

--- application/app/controllers/patients_controller.rb
+++ application/app/controllers/patients_controller.rb
@@ -380,8 +380,11 @@ class PatientsController < ApplicationController
 
   def update_demographics
     Person.update_demographics(params)
-    if HsaVillage.is_patient_village_in_anc_connect(params[:person_id],session[:district])
-      anc_connect_program_id = Program.find_by_name("ANC CONNECT PROGRAM").program_id
+    anc_connect_program_id = Program.find_by_name("ANC CONNECT PROGRAM").program_id
+    patient_anc_program = PatientProgram.find(:last, :conditions => ["patient_id =? AND
+                 program_id=?", params[:person_id], anc_connect_program_id])
+    
+    if HsaVillage.is_patient_village_in_anc_connect(params[:person_id],session[:district]) && patient_anc_program.present?
       anc_patient_program = PatientProgram.find_by_patient_id_and_program_id_and_voided(params[person_id],anc_connect_program_id,0)
       if anc_patient_program.present?
         anc_patient_program.void("Removed from ANC connect program")
