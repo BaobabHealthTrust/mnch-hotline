792fd85e187fc9838fc310182201ff45c7f6fb25
blessings <blessings.mhango@gmail.com>
2014-12-15 09:52:21 +0200

    added county district

--- application/app/controllers/clinic_controller.rb
+++ application/app/controllers/clinic_controller.rb
@@ -397,7 +397,8 @@ class ClinicController < ApplicationController
           follow_up[:family_name_prefix] = person.family_name_prefix
           follow_up[:current_residence] = person.city_village
           follow_up[:gestation_age] = person.gestation_age
-          village = Village.find_by_name_and_county_district(person.city_village, person.county_district)
+          traditional_authority_id = TraditionalAuthority.find_by_name(person.county_district).traditional_authority_id
+          village = Village.find_by_name_and_traditional_authority_id(person.city_village, traditional_authority_id)
           hsa_village = HsaVillage.find_by_village_id(village.village_id) rescue nil
           person_name = PersonName.find_by_person_id(hsa_village.hsa_id) rescue nil
           follow_up[:hsa_name] = person_name.given_name + " " + person_name.family_name rescue nil
