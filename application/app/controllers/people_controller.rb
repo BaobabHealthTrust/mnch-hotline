33a53a91d31359147d6085149a9a050e0a111b96
Deliwe <dbnkhoma@gmail.com>
2015-04-14 15:23:36 +0200

    edited edit_demographics form

--- application/app/controllers/people_controller.rb
+++ application/app/controllers/people_controller.rb
@@ -110,9 +110,36 @@ class PeopleController < ApplicationController
       redirect_to :action => 'search', :identifier => "#{params[:national_id]}" and return
     end
   end
+
+  # Districts containing the string given in params[:value]
+  def district
+    #region_id = Region.find_by_name("#{params[:filter_value]}").id
+    region_conditions = ["name LIKE (?) AND region_id <> 4", "#{params[:search_string]}%"]
+
+    districts = District.find(:all,:conditions => region_conditions, :order => 'name')
+    districts = districts.map do |d|
+      "<li value='#{d.name}'>#{d.name}</li>"
+    end
+    render :text => districts.join('') + "<li value='Other'>Other</li>" and return
+  end
   
   def traditional_authority
+
     district_id = District.find_by_name("#{session[:district]}").id
+    
+    traditional_authority_conditions = ["name LIKE (?) AND district_id = ?", "%#{params[:search_string]}%", district_id]
+
+    traditional_authorities = TraditionalAuthority.find(:all,:conditions => traditional_authority_conditions, :order => 'name')
+    traditional_authorities = traditional_authorities.map do |t_a|
+      "<li value='#{t_a.name}'>#{t_a.name}</li>"
+    end
+    render :text => traditional_authorities.join('') + "<li value='Other'>Other</li>" and return
+  end
+
+  def traditional_authority_demographics
+
+    district_id = District.find_by_name("#{params[:filter_value]}").id
+
     traditional_authority_conditions = ["name LIKE (?) AND district_id = ?", "%#{params[:search_string]}%", district_id]
 
     traditional_authorities = TraditionalAuthority.find(:all,:conditions => traditional_authority_conditions, :order => 'name')
@@ -134,8 +161,11 @@ class PeopleController < ApplicationController
   end
     
   def village_demographics
-  
-    village_conditions = ["name LIKE (?)", "%#{params[:search_string]}%"]
+    
+    traditional_authority_id = TraditionalAuthority.find_by_name("#{params[:filter_value]}").id
+   
+    village_conditions = ["name LIKE (?) AND traditional_authority_id = ?", "%#{params[:search_string]}%", traditional_authority_id]
+    #village_conditions = ["name LIKE (?)", "%#{params[:search_string]}%"]
 
     villages = Village.find(:all, :select => "DISTINCT name", :conditions => village_conditions, :order => 'name', :limit => 10)
     village = villages.uniq rescue []
